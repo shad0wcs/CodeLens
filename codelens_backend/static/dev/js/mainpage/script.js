91dd8c645afaa815e46fa16618906dbff5c3b5c3
Aleksandr Kolotov <kolotov2004@list.ru>
2024-11-19 02:11:46 +0300

    now you can see the name of the downloaded file

--- codelens_backend/static/dev/js/mainpage/script.js
+++ codelens_backend/static/dev/js/mainpage/script.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+// Копирование кода
 function copyToClipboard() {
     const resultBox = document.getElementById("resultBox"); // Ссылка на блок с кодом
     const textToCopy = resultBox.innerText || resultBox.textContent; // Получаем содержимое блока
@@ -24,11 +25,25 @@ function copyToClipboard() {
             const originalContent = resultBox.innerHTML;
             resultBox.innerHTML = "<p>Скопировано в буфер обмена!</p>"; // Уведомление
             setTimeout(() => {
-                resultBox.innerHTML = originalContent; // Возвращаем оригинальный текст через 1.5 сек
+                resultBox.innerHTML = originalContent;
             }, 1000);
         })
         .catch(err => {
             console.error("Ошибка копирования: ", err);
             alert("Не удалось скопировать. Попробуйте снова.");
         });
+}
+
+// Имя файла
+const fileInput = document.querySelector("input[type='file']");
+const fileNameDisplay = document.getElementById("fileNameDisplay");
+
+if (fileInput) {
+    fileInput.addEventListener("change", function () {
+        if (this.files && this.files[0]) {
+            fileNameDisplay.textContent = this.files[0].name;
+        } else {
+            fileNameDisplay.textContent = "Файл не выбран";
+        }
+    });
 }
